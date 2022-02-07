fbdc5094005e16e1bafa2d049874d3ab0723897e
Justin Mahar <contact@justinmahar.com>
2022-02-07 07:09:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { IGEmbed } from "react-ig-embed";
 
 function App() {
+  const [show, setShow] = React.useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      {show && (
+        <IGEmbed
+          url="https://www.instagram.com/p/CUbHfhpswxt/"
+          style={{ width: 450 }}
+          retryDisabled
+        />
+      )}
     </div>
   );
 }
